0a75d0f37324f5cc9543344814d0f302d22fc450
Ritvik Bhawnani <ritvikbhawnani@gmail.com>
2019-07-02 23:30:58 +0400

    Improved send feedback functionality

--- app/src/main/java/com/techaura/candycalculator/SettingsFragment.java
+++ app/src/main/java/com/techaura/candycalculator/SettingsFragment.java
@@ -1,7 +1,12 @@
 package com.techaura.candycalculator;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -35,22 +40,56 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                 getResources().getColor(R.color.blueColorPrimary)};
 
         ColorPickerPreference colorPickerPreference = findPreference("color_picker");
-        if (colorPickerPreference == null) return;
-        colorPickerPreference.setColors(colors);
+        if (colorPickerPreference != null) {
+            colorPickerPreference.setColors(colors);
 
-        if (getContext() != null) {
-            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
-            SharedPreferences.Editor editor = sharedPreferences.edit();
+            if (getContext() != null) {
+                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+                SharedPreferences.Editor editor = sharedPreferences.edit();
 
-            colorPickerPreference.setOnPreferenceChangeListener((Preference preference, Object newValue) -> {
-                String theme = ThemeUtils.identifyTheme(Integer.parseInt(newValue.toString()));
+                colorPickerPreference.setOnPreferenceChangeListener((Preference preference, Object newValue) -> {
+                    String theme = ThemeUtils.identifyTheme(Integer.parseInt(newValue.toString()));
 
-                editor.putString("current_theme", theme);
-                editor.apply();
+                    editor.putString("current_theme", theme);
+                    editor.apply();
 
-                if (getActivity() != null) getActivity().recreate();
+                    if (getActivity() != null) getActivity().recreate();
+                    return true;
+                });
+            }
+
+        }
+
+        Preference feedbackPreference = findPreference("send_feedback");
+        if (feedbackPreference != null) {
+            feedbackPreference.setOnPreferenceClickListener((Preference preference) -> {
+                if (getActivity() != null) sendFeedback(getActivity());
                 return true;
             });
+        }
+
+    }
+
+    private void sendFeedback(Context context) {
+        String body = null;
+
+        try {
+            body = context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionName;
+            body = "\n\n-----------------------------\nPlease don't remove this information\n\n Device OS: Android \n Device OS version: " +
+                    Build.VERSION.RELEASE + "\n App Version: " + body + "\n Device Brand: " + Build.BRAND +
+                    "\n Device Model: " + Build.MODEL + "\n Device Manufacturer: " + Build.MANUFACTURER;
+        } catch (PackageManager.NameNotFoundException e) {
+            body = "Please enter the following information\n\n Device OS\n Device OS Version\n Device Brand\n Device Model\n App Version";
+        }
+
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{context.getString(R.string.feedback_summary)});
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Candy Calculator Feedback");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, body);
+
+        if (emailIntent.resolveActivity(context.getPackageManager()) != null) {
+            startActivity(emailIntent);
         }
 
     }
